e2e2b804a21ea3dd88dc6df288713f3ffd470783
Cristian Nuñez <cristian15nz@gmail.com>
2020-07-21 11:01:04 -0400

    Cambios menores.

--- actor.php
+++ actor.php
@@ -3,18 +3,50 @@
 require_once "funciones/ayudante.php";
 require_once "modelos/modelo_actor.php";
 
+// Declarar las variables
 $nombrePagina = "Actor";
 
-// Declarar las variables
-$nombreActor = $_GET['nombreActor'] ?? "";
-$apellidoActor = $_GET['apellidoActor'] ?? "";
+$nombreActor = $_POST['nombreActor'] ?? "";
+$apellidoActor = $_POST['apellidoActor'] ?? "";
 
-// Asegurarnos de que el usuario haya hecho click en el boton Guardar Actor
-if (isset($_GET['guardar_actor'])) {
-    // codigo para guardar en la base de datos
-    echo "Se van a guardar los datos...";
+//imprimirArray($_POST);
+
+try {
+
+
+    // Asegurarnos de que el usuario haya hecho click en el boton Guardar Actor
+    if ( isset($_POST['guardar_actor']) ) {
+
+        // Validar datos
+        if ( empty($nombreActor) ) {
+            throw new Exception("El nombre no puede estar vacío.");
+        }
+
+        if ( empty($apellidoActor) ) {
+            throw new Exception("El apellido no puede estar vacío.");
+        }
+
+        // Preparar el array con los datos
+        $datos = compact('nombreActor', 'apellidoActor');
+
+        // Insertar los datos
+        $actorInsertado = insertarActores($conexion, $datos);
+        $mensaje = "Los datos del actor se guardaron correctamente.";
+
+        // Lanzar un error si no se insertó correctamente
+        if ( ! $actorInsertado ) {
+            throw new Exception("Ocurrió un error al insertar los datos del actor");
+        }
+
+        // Redireccionar la pagina
+        redireccionar("actor.php");
+
+    }
+} catch ( Exception $e ) {
+    $error = $e->getMessage();
 }
 
+// Cargar los datos de los modelos
 $actores = obtenerActores($conexion);
 
 // Incluir la vista
